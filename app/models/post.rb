bc0b6593b3ceb27d412332a9d9928f1772e83314
krisbobo <kiyongbohbo@gmail.com>
2023-06-24 19:29:09 +0100

    Add validations for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,12 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  after_save :increment_user_posts_counter
-  after_destroy :decrement_user_posts_counter
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :increment_author_posts_counter
+  after_destroy :decrement_author_posts_counter
 
   after_create :update_likes_counter
   after_destroy :update_likes_counter
@@ -19,7 +23,11 @@ class Post < ApplicationRecord
     author.increment!(:posts_counter)
   end
 
-  def decrement_user_posts_counter
+  def decrement_author_posts_counter
     author.decrement!(:posts_counter)
+  end
+
+  def update_likes_counter
+    update(likes_counter: likes.count)
   end
 end
