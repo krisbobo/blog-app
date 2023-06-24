93ce1e9b39a6fc3cfe3fbc058a706961acca92b3
krisbobo <kiyongbohbo@gmail.com>
2023-06-24 13:30:16 +0100

    Add associations and method to update the likes counter for a post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,17 @@
 class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_save :increment_likes_counter
+  after_destroy :decrement_likes_counter
+
+  private
+
+  def increment_likes_counter
+    post.increment!(:likes_counter)
+  end
+
+  def decrement_likes_counter
+    post.decrement!(:likes_counter)
+  end
 end
