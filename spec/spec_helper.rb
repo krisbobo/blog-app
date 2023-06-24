e35326550354e7c3c874f4a35f7705f30d73bea4
Christian Kiyong <kiyongbohbo@gmail.com>
2023-06-24 23:35:56 +0100

    Fix linters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -88,4 +88,5 @@ RSpec.configure do |config|
 #   # Setting this allows you to use `--seed` to deterministically reproduce
 #   # test failures related to randomization by passing the same `--seed` value
 #   # as the one that triggered the failure.
+
 end
