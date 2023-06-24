59c3cbaaefa09d5943944e53cca3e32adc1df907
Christian Kiyong <kiyongbohbo@gmail.com>
2023-06-24 23:34:45 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -88,5 +88,4 @@ RSpec.configure do |config|
 #   # Setting this allows you to use `--seed` to deterministically reproduce
 #   # test failures related to randomization by passing the same `--seed` value
 #   # as the one that triggered the failure.
-#   # Kernel.srand config.seed
 end
