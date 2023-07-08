c66729e1c102e99a48c16b3f3fb72c787a00c4c2
krisbobo <kiyongbohbo@gmail.com>
2023-07-08 16:17:09 +0100

    Create controller methods to use and create new user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
     author_id = params[:author_id]
     @posts = @posts.where(author_id:) if author_id.present?
     @comments = @posts.map(&:recent_comments)
+    @comments = Comment.all
     render :index
   end
 
@@ -12,10 +13,37 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
     @user = User.find_by(id: params[:user_id])
     @comments = @post.recent_comments if @post
+    @comment = Comment.where(post_id: params[:id])
     if @post
       render :show
     else
       redirect_to posts_url
     end
+  end
+
+  def new
+    @user = @current_user
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    @user = @current_user
+    @post = Post.new(post_params)
+    @post.author_id = @user.id
+
+    if @post.save
+      flash[:success] = 'Post successfully created'
+      redirect_to user_posts_url(@post.author_id)
+    else
+      flash[:alert] = 'Post could not be created'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text).merge(author_id: @user.id)
   end
 end
