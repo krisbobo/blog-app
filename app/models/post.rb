ab36806e9f4a2e67554835f5b56496a22e2a4ed4
krisbobo <kiyongbohbo@gmail.com>
2023-07-08 16:14:11 +0100

    Update file

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_save :increment_author_posts_counter
+  after_create :increment_author_posts_counter
   after_destroy :decrement_author_posts_counter
 
   after_create :update_likes_counter
