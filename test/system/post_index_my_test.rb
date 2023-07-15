d192e7a646fb159b3bdc2ff3f995721c06bef8a1
Syed Atril <atril3123@gmail.com>
2023-07-15 09:27:29 +0500

    Update post_index_my_test.rb

--- test/system/post_index_my_test.rb
+++ test/system/post_index_my_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class PostIndexMyTest < ActionDispatch::IntegrationTest
   test 'displays user post index correctly' do
     user = users(:one)
-    Post.create(title: 'Post 1', text: 'Post 5 text', comments_counter: 5, like_counter: 10, author: user)
-    Post.create(title: 'Post 2', text: 'Post 12 text', comments_counter: 3, like_counter: 7, author: user)
+    Post.create(title: 'Post 1', text: 'Post 1 text', comments_counter: 5, likes_counter: 10, author: user)
+    Post.create(title: 'Post 2', text: 'Post 2 text', comments_counter: 3, likes_counter: 7, author: user)
 
     get user_posts_path(user)
     assert_response :success
 
-    assert_select '.bttn_3', text: 'Pagination'
+    assert_select '.btn_3', text: 'Pagination'
   end
 end
